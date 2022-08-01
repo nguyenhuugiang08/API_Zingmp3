3206d439f9c6c51c17b0d174cfd5e5df256d75e6
Nguyen Huu Giang <nguyenhuugiangtb08012001@gmail.com>
2022-08-01 17:10:35 +0700

    update

--- src/index.js
+++ src/index.js
@@ -3,18 +3,7 @@ const app = express();
 const cors = require('cors');
 const route = require('./routes')
 
-var whitelist = ['http://localhost:3000', 'https://zing-clone-react-app.herokuapp.com', 'https://api-zing-clone.herokuapp.com/']
-var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
-
-app.use(cors(corsOptions));
+app.use(cors());
 
 route(app);
 
